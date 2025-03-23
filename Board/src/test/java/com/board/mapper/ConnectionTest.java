6a15f59bce2a8880d1bd2ff5c0d79457be5f7d79
YB-Taekwon <loundo001@gmail.com>
2025-03-24 02:19:05 +0900

    프로젝트 생성

---
+++ Board/src/test/java/com/board/mapper/ConnectionTest.java
@@ -0,0 +1,30 @@
+package com.board.mapper;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class ConnectionTest {
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testConnection() {
+		try (Connection con = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/com?serverTimezone=UTC",
+				"com", "com01")) {
+			System.out.println(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+
+	}
+}
